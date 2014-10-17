f8a8ef0aad07d10d98cda91416ae876525772381
Maarten Versteegh <maartenversteegh@gmail.com>
2014-10-17 15:51:22 +0200

    embed_eval for aws

--- embed_eval.py
+++ embed_eval.py
@@ -9,6 +9,8 @@ import operator
 import os
 from collections import defaultdict
 
+import joblib
+
 from scipy.spatial.distance import pdist, cdist
 from scipy.io import loadmat
 import numpy as np
@@ -19,10 +21,10 @@ from nnet_archs import CrossNet
 from dataset_iterators import CrossLearnIterator
 
 CACHE_DIR = '/fhgfs/bootphon/scratch/mwv/speech2image/cache/'
-try:
-    os.makedirs(CACHE_DIR)
-except OSError:
-    pass
+# try:
+#     os.makedirs(CACHE_DIR)
+# except OSError:
+#     pass
 
 def tridist_idx(d, i, j, n):
     if j > i:
@@ -166,37 +168,108 @@ def image_search_by_word_query(query_snd, multi_distances, snd_fnames, img_fname
             break
     return rank
 
-def image_search_recall_at_k(k, query_snd, multi_distances, snd_fnames, img_fnames, pic2id):
-    """is there a hit among the closest k image neighbors for the query sound?"""
-    image_neighbors = np.argsort(multi_distances[:, query_snd])[:k]
-    query_id = snd_fnames[query_snd]
-    for i in xrange(k):
-        if query_id in pic2id[img_fnames[image_neighbors[i]]]:
-            return True
-    return False
-
-
-def word_search_by_image_query(query_img, multi_distances, snd_fnames, img_fnames, id2pic):
-    """map an image query into the embedding space and find words in the same space.
-    return rank of first word that is in the tokenlist of the image"""
-    word_neighbors = np.argsort(multi_distances[query_img, :])
-    query_pic = img_fnames[query_img]
+def image_TOKEN_search_by_word_query_TOKEN(query_snd_ix, multi_distances,
+                                           snd_fnames, img_fnames,
+                                           id2pic):
+    """map a word token query into the embedding space and find images in the same space
+    return rank of first neighbor whose TOKEN is in the picture list of the id"""
     n_images, n_sounds = multi_distances.shape
-    rank = n_images
-    for i in xrange(word_neighbors.shape[0]):
-        if query_pic in id2pic[snd_fnames[word_neighbors[i]]]:
-            rank = i+1
+    query_id = snd_fnames[query_snd_ix]
+
+    img_neighbors = np.argsort(multi_distances[:, query_snd_ix])
+
+    pictures_for_query = id2pic[query_id]
+
+    rank = img_neighbors.shape[0]
+    for i in xrange(img_neighbors.shape[0]):
+        if img_fnames[img_neighbors[i]] in pictures_for_query:
+            rank = i + 1
             break
     return rank
 
-def word_search_recall_at_k(k, query_img, multi_distances, snd_fnames, img_fnames, id2pic):
-    """is there a hit among the closest k sound neighbors for query image?"""
-    word_neighbors = np.argsort(multi_distances[query_img, :])[:k]
-    query_pic = img_fnames[query_img]
-    for i in xrange(k):
-        if query_pic in id2pic[snd_fnames[word_neighbors[i]]]:
-            return True
-    return False
+
+def image_TOKEN_search_by_word_query_TYPE_at_k(k, word_TYPE, multi_distances,
+                                               snd_fnames, img_fnames,
+                                               word2id, id2pic):
+    """map all word tokens corresponding to word_TYPE into the embedding space and take
+    the k nearest neighbors for each token. concatenate the neighbors. return rank of the
+    first neighbor whose token is in the concatenated picture lists"""
+    n_images, n_sounds = multi_distances.shape
+    word_tokens = word2id[word_TYPE]
+    word_tokens &= set(snd_fnames)
+
+    if len(word_tokens) == 0:
+        raise ValueError('not enough word tokens for {0}'.format(word_TYPE))
+
+    pictures_for_word = set()
+    for token in word_tokens:
+        pictures_for_word.update(id2pic[token])
+
+    id2ix = {v: ix for ix, v in enumerate(snd_fnames)}
+
+    image_token_ix = None
+    image_token_dists = None
+
+    for token in word_tokens:
+        # find k neighbors, keep their ix and distance
+        nn = multi_distances[:, id2ix[token]]
+        nn_ix = np.argsort(nn)[:k]
+        nn_dists = nn[nn_ix]
+        if image_token_ix is None:
+            image_token_ix = nn_ix
+            image_token_dists = nn_dists
+        else:
+            image_token_ix = np.hstack((image_token_ix, nn_ix))
+            image_token_dists = np.hstack((image_token_dists, nn_dists))
+
+    sort_ix = np.argsort(image_token_dists)
+    rank = sort_ix.shape[0]
+    for ix in sort_ix:
+        if img_fnames[image_token_ix[ix]] in pictures_for_word:
+            rank = ix + 1
+            break
+    return rank
+
+
+def word_TOKEN_search_by_image_query_TOKEN(query_img_ix, multi_distances,
+                                           snd_fnames, img_fnames,
+                                           pic2id):
+    """map an image query into the embedding space and find words in the same space.
+    return rank of first neighbor whose TOKEN is in the tokenlist of the image"""
+    n_images, n_sounds = multi_distances.shape
+
+    word_neighbors = np.argsort(multi_distances[query_img_ix, :])
+    query_pic = img_fnames[query_img_ix]
+    tokens_for_query = pic2id[query_pic]
+
+    rank = n_sounds
+    for i in xrange(word_neighbors.shape[0]):
+        if snd_fnames[word_neighbors[i]] in tokens_for_query:
+            rank = i + 1
+            break
+    return rank
+
+def word_TYPE_search_by_image_query_TOKEN(query_img_ix, multi_distances,
+                                          snd_fnames, img_fnames,
+                                          pic2id, id2word):
+    """map an image query into the embedding space and find words in the same space.
+    return rank of first neighbor whose TYPE is in the tokenlist of the image"""
+    n_images, n_sounds = multi_distances.shape
+
+    word_neighbors = np.argsort(multi_distances[query_img_ix, :])
+    query_pic = img_fnames[query_img_ix]
+    tokens_for_query = pic2id[query_pic]
+    words_for_query = set([id2word[x] for x in tokens_for_query])
+
+    rank = n_sounds
+    for i in xrange(word_neighbors.shape[0]):
+        if id2word[snd_fnames[word_neighbors[i]]] in words_for_query:
+            rank = i + 1
+            break
+    return rank
+
+
+
 
 
 def mapping_dicts(lucid_tokens_df, corpus_df, lucid2pascal):
@@ -223,19 +296,22 @@ def mapping_dicts(lucid_tokens_df, corpus_df, lucid2pascal):
     return id2word, word2id, id2pic, pic2id
 
 def load_net(nnet_file):
-    with open(nnet_file, 'rb') as fid:
-        nnet = pickle.load(fid)
+    if path.splitext(nnet_file)[1] == 'joblib':
+        nnet = joblib.load(nnet_file)
+    else:
+        with open(nnet_file, 'rb') as fid:
+            nnet = pickle.load(fid)
     return nnet
 
 
 if __name__ == '__main__':
-    dataset_path = "/fhgfs/bootphon/scratch/gsynnaeve/learning_semantics2014/pascal_full/"
-
-    if len(sys.argv) != 6:
-        print 'usage: embed_eval.py NNET_FILE CORPUS_FILE TOKEN_FILE STACKTOKENSDIR REPLACEMENTFILE'
-        exit()
+    datadir = '/mnt/data/pascal1k_prepared_dataset/'
+    stackdir = path.join(LUCID_stack_tokens_drop)
+    # if len(sys.argv) != 6:
+    #     print 'usage: embed_eval.py NNET_FILE CORPUS_FILE TOKEN_FILE STACKTOKENSDIR REPLACEMENTFILE'
+    #     exit()
     print 'loading net...',
-    nnet_file = sys.argv[1]
+    nnet_file = 'full_pascal1k_crossnet_adadelta_emb_200.pickle'
     nnet = load_net(nnet_file)
 
     transform_imgs = nnet.transform_img()
@@ -244,30 +320,32 @@ if __name__ == '__main__':
     print 'done.'
 
     print 'loading corpus...',
-    corpus_file = sys.argv[2]
+    corpus_file = path.join(datadir, 'corpus_drop.pkl')
     corpus_df = pd.read_pickle(corpus_file)
     print 'done.'
 
     print 'loading tokens file...',
-    tokens_file = sys.argv[3]
+    tokens_file = path.join(datadir, 'lucid_tokens_drop.pkl')
     tokens_df = pd.read_pickle(tokens_file)
     print 'done.'
 
     print 'loading mappings...',
-    replacement_file = sys.argv[5]
+    replacement_file = path.join(datadir, 'replacements.txt')
     lucid2pascal = dict(x.strip().split('\t')[::-1]
+                        for x in open(replacement_file))
+    pascal2lucid = dict(x.strip().split('\t')
                         for x in open(replacement_file))
     id2word, word2id, id2pic, pic2id = mapping_dicts(tokens_df, corpus_df, lucid2pascal)
     print 'done.'
 
     print 'loading images...',
-    img_mat_fname = path.join(dataset_path, 'split_test_img.mat')
+    img_mat_fname = '/mnt/data/pascal1k_prepared_dataset/split_test_img.mat')
     img_fnames, img_X = load_images(img_mat_fname, corpus_df, force_rebuild=True)
     pic2ix = {v:k for k, v in enumerate(img_fnames)}
     print 'done.'
 
     print 'loading sounds...',
-    stack_tokens_dir = sys.argv[4]
+    stack_tokens_dir = '/mnt/data/pascal1k_prepared_dataset/LUCID_stack_tokens_drop/'
     snd_fnames, snd_X = load_sounds(img_fnames, corpus_df, stack_tokens_dir, force_rebuild=True)
     sndID2ix = {v:k for k, v in enumerate(snd_fnames)}
     print 'done.'
@@ -312,47 +390,103 @@ if __name__ == '__main__':
     print 'word similarity for image (median, mean rank): {0:.3f} {1:.3f}'.format(
         np.median(word_similarity_ranks), word_similarity_ranks.mean())
 
+
+    # 2. word TOKEN search by image TOKEN
+    word_token_image_token_ranks = np.array(
+        [word_TOKEN_search_by_image_query_TOKEN(i, multi_dists, snd_fnames, img_fnames, pic2id)
+         for i in xrange(len(img_fnames))])
+
+    print 'word TOKEN search by image TOKEN (median, mean rank): {0:.3f} {1:.3f}'.format(
+        np.median(word_token_image_token_ranks), word_token_image_token_ranks.mean())
+
+    # 3. word TYPE search by image TOKEN
+    word_type_image_token_ranks = np.array(
+        [word_TYPE_search_by_image_query_TOKEN(i, multi_dists, snd_fnames, img_fnames, pic2id, id2word)
+         for i in xrange(len(img_fnames))])
+
+    print 'word TYPE search by image TOKEN (median, mean rank): {0:.3f} {1:.3f}'.format(
+        np.median(word_type_image_token_ranks), word_type_image_token_ranks.mean())
+
+    # 4. image TOKEN search by word TOKEN
+    image_token_word_token_ranks = np.array(
+        [image_TOKEN_search_by_word_query_TOKEN(i, multi_dists, snd_fnames, img_fnames, id2pic)
+         for i in xrange(len(snd_fnames))])
+
+    print 'image TOKEN search by word TOKEN (median, mean rank): {0:.3f} {1:.3f}'.format(
+        np.median(image_token_word_token_ranks), image_token_word_token_ranks.mean())
+
+    # 5. image TYPE search by word TOKEN
+
+    # RECALL AT K
+    for k in [1, 5, 10, 25, 50]:
+        word_token_image_token_recall = \
+            100 * np.sum(word_token_image_token_ranks <= k) / len(word_token_image_token_ranks)
+        print 'word token search by image token recall at {0:d}: {1:.3f}'.format(
+            k, word_token_image_token_recall)
+    for k in [1, 5, 10, 25, 50]:
+        word_type_image_token_recall = \
+            100 * np.sum(word_type_image_token_ranks <= k) / len(word_type_image_token_ranks)
+        print 'word type search by image token recall at {0:d}: {1:.3f}'.format(
+            k, word_type_image_token_recall)
+    for k in [1, 5, 10, 25, 50]:
+        image_token_word_token_recall = \
+            100 * np.sum(image_token_word_token_ranks <= k) / len(image_token_word_token_ranks)
+        print 'image token search by word token recall at {0:d}: {1:.3f}'.format(
+            k, image_token_word_token_recall)
+    # for k in [1, 5, 10, 25, 50]:
+    image_token_word_type_ranks = \
+        np.array([image_TOKEN_search_by_word_query_TYPE_at_k(200, word, multi_dists,
+                                                             snd_fnames, img_fnames,
+                                                             word2id, id2pic)
+                  for word in [lucid2pascal.get(id2word[ID], id2word[ID])
+                               for ID in set(snd_fnames)]])
+    for k in [1, 5, 10, 25, 50]:
+        image_token_word_type_recall = \
+            100 * np.sum(image_token_word_type_ranks <= k) / len(image_token_word_type_ranks)
+        print 'image token search by word type recall at {0:d}: (median, mean) {1:.3f}'.format(
+            k, image_token_word_type_recall)
+
     # 2. image search with query words
-    image_search_ranks = np.array([image_search_by_word_query(i,
-                                                              multi_dists,
-                                                              snd_fnames,
-                                                              img_fnames,
-                                                              pic2id)
-                                   for i in xrange(len(snd_fnames))])
-    print 'image search by word query (median, mean rank): {0:.3f} {1:3f}'.format(
-        np.median(image_search_ranks), image_search_ranks.mean())
+    # image_search_ranks = np.array([image_search_by_word_query(i,
+    #                                                           multi_dists,
+    #                                                           snd_fnames,
+    #                                                           img_fnames,
+    #                                                           pic2id)
+    #                                for i in xrange(len(snd_fnames))])
+    # print 'image search by word query (median, mean rank): {0:.3f} {1:3f}'.format(
+    #     np.median(image_search_ranks), image_search_ranks.mean())
 
-    # 3. word search with query image
-    word_search_ranks = np.array([word_search_by_image_query(i,
-                                                             multi_dists,
-                                                             snd_fnames,
-                                                             img_fnames,
-                                                             id2pic)
-                                  for i in xrange(len(img_fnames))])
-    print 'word search by image query (median, mean rank): {0:.3f} {1:.3f}'.format(
-        np.median(word_search_ranks), word_search_ranks.mean())
+    # # 3. word search with query image
+    # word_search_ranks = np.array([word_search_by_image_query_token(i,
+    #                                                                multi_dists,
+    #                                                                snd_fnames,
+    #                                                                img_fnames,
+    #                                                                id2pic)
+    #                               for i in xrange(len(img_fnames))])
+    # print 'word search by image query (median, mean rank): {0:.3f} {1:.3f}'.format(
+    #     np.median(word_search_ranks), word_search_ranks.mean())
 
-    # 4. recall at k
+    # # 4. recall at k
 
-    for k in [1, 5, 10, 25, 50]:
-        image_search_recall = image_search_ranks <= k
-        # image_search_recall = np.array([image_search_recall_at_k(k,
-        #                                                          i,
-        #                                                          multi_dists,
-        #                                                          snd_fnames,
-        #                                                          img_fnames,
-        #                                                          pic2id)
-        #                                 for i in xrange(len(snd_fnames))])
-        print 'image search recall at {0}: {1:.3f}'.format(
-            k, np.sum(image_search_recall) * 100 / len(image_search_recall))
-    for k in [1, 5, 10, 25, 50]:
-        word_search_recall = word_search_ranks <= k
-        # word_search_recall = np.array([word_search_recall_at_k(k,
-        #                                                        i,
-        #                                                        multi_dists,
-        #                                                        snd_fnames,
-        #                                                        img_fnames,
-        #                                                        pic2id)
-        #                                for i in xrange(len(img_fnames))])
-        print 'word search recall at {0}: {1:.3f}'.format(
-            k, np.sum(word_search_recall) * 100 / len(word_search_recall))
+    # for k in [1, 5, 10, 25, 50]:
+    #     image_search_recall = image_search_ranks <= k
+    #     # image_search_recall = np.array([image_search_recall_at_k(k,
+    #     #                                                          i,
+    #     #                                                          multi_dists,
+    #     #                                                          snd_fnames,
+    #     #                                                          img_fnames,
+    #     #                                                          pic2id)
+    #     #                                 for i in xrange(len(snd_fnames))])
+    #     print 'image search recall at {0}: {1:.3f}'.format(
+    #         k, np.sum(image_search_recall) * 100 / len(image_search_recall))
+    # for k in [1, 5, 10, 25, 50]:
+    #     word_search_recall = word_search_ranks <= k
+    #     # word_search_recall = np.array([word_search_recall_at_k(k,
+    #     #                                                        i,
+    #     #                                                        multi_dists,
+    #     #                                                        snd_fnames,
+    #     #                                                        img_fnames,
+    #     #                                                        pic2id)
+    #     #                                for i in xrange(len(img_fnames))])
+    #     print 'word search recall at {0}: {1:.3f}'.format(
+    #         k, np.sum(word_search_recall) * 100 / len(word_search_recall))
